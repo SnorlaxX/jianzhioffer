48cfd335e6c49f490603d799972ba73bc866580d
snorlax <1175937870@qq.com>
2021-03-06 00:37:35 +0800

    开始第31题

---
+++ JZ31/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int NumberOf1Between1AndN_Solution(int n) 
+    {
+    
+    }
+};
+
+int main()
+{
+    Solution s;
+    int res = s.NumberOf1Between1AndN_Solution(13);
+    cout << res << endl;
+    system("pause");
+    return 0;
+}
