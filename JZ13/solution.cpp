4e5fb29ae0a9fa548fe21ffae517ba1476dfca57
snorlax <1175937870@qq.com>
2021-03-10 21:23:43 +0800

    第13题新增一种做法

--- JZ13/solution.cpp
+++ JZ13/solution.cpp
@@ -5,6 +5,8 @@ using namespace std;
 class Solution {
 public:
     vector<int> reOrderArray(vector<int>& array) {
+        /*
+        **类似于冒泡排序的写法
         for(int i = 0; i<array.size();i++){
             for(int j = 0; j<array.size()-1-i; j++){
                 if((array[j] % 2 == 0) && (array[j+1] % 2 == 1)){
@@ -15,6 +17,19 @@ public:
             }
         }
         return array;
+        */
+        vector<int> res;
+        for (int i = 0; i < array.size(); i++){
+            if(array[i] % 2 == 1){
+                res.push_back(array[i]);
+            }
+        }
+        for (int i = 0; i < array.size(); i++){
+            if(array[i] % 2 == 0){
+                res.push_back(array[i]);
+            }
+        }
+        return res;
     }
 };
 
